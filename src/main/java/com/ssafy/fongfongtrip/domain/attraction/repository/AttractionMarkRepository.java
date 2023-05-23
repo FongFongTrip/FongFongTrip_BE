5c4b1b522469008354336c3fb915770b936988f1
wancern <wancern@naver.com>
2023-05-23 17:35:11 +0900

    fix: unused import statement 제거

--- src/main/java/com/ssafy/fongfongtrip/domain/attraction/repository/AttractionMarkRepository.java
+++ src/main/java/com/ssafy/fongfongtrip/domain/attraction/repository/AttractionMarkRepository.java
@@ -2,7 +2,6 @@ package com.ssafy.fongfongtrip.domain.attraction.repository;
 
 import com.ssafy.fongfongtrip.domain.attraction.entity.AttractionInfo;
 import com.ssafy.fongfongtrip.domain.attraction.entity.AttractionMark;
-import com.ssafy.fongfongtrip.domain.board.entity.Board;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
