f1aaa55087414ba429ea2e060b984e865665ce7d
wancern <wancern@naver.com>
2023-05-23 16:05:00 +0900

    fix: unused import statement 제거

--- src/main/java/com/ssafy/fongfongtrip/domain/board/repository/BoardMarkRepository.java
+++ src/main/java/com/ssafy/fongfongtrip/domain/board/repository/BoardMarkRepository.java
@@ -3,14 +3,11 @@ package com.ssafy.fongfongtrip.domain.board.repository;
 import com.ssafy.fongfongtrip.domain.board.entity.Board;
 import com.ssafy.fongfongtrip.domain.board.entity.BoardMark;
 import com.ssafy.fongfongtrip.domain.board.entity.BoardMarkId;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface BoardMarkRepository extends JpaRepository<BoardMark, BoardMarkId> {
 
