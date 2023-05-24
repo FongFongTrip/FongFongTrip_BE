01b6d69690a7d002777c303e8c621bf52a19cd72
wancern <wancern@naver.com>
2023-05-24 14:48:47 +0900

    fix: 북마크 api 정렬조건 추가

--- src/main/java/com/ssafy/fongfongtrip/domain/board/repository/BoardMarkRepository.java
+++ src/main/java/com/ssafy/fongfongtrip/domain/board/repository/BoardMarkRepository.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 public interface BoardMarkRepository extends JpaRepository<BoardMark, BoardMarkId> {
 
-    @Query(value = "select b from Board b, BoardMark m where b.id = m.id.board.id and m.id.member.id = :memberId ",
+    @Query(value = "select b from Board b, BoardMark m where b.id = m.id.board.id and m.id.member.id = :memberId order by b.isNotice desc , b.created desc ",
             countQuery = "select count(b) from Board b")
     List<Board> findMarkByMemberId(@Param("memberId") Long memberId);
 }
